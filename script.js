bfe4098a37d7080b6fbd0460e476c36902c7bf88
Casper <casp8249@edu.easj.dk>
2022-05-10 15:01:09 +0200

    Brand Intro text + menu

---
+++ script.js
@@ -0,0 +1,9 @@
+let tabs = document.getElementById("menu-tabs");
+
+function menutabs() {
+  if (tabs.style.display === "none") {
+    tabs.style.display = "flex";
+  } else {
+    tabs.style.display = "none";
+  }
+}
